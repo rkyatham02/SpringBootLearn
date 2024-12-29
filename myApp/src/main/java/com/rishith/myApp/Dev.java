c1b19a40305e8a49733bfc9177c8e2466c459568
rkyatham02 <rkyatham02@gmail.com>
2024-12-29 15:00:07 -0500

    added autowiring

--- myApp/src/main/java/com/rishith/myApp/Dev.java
+++ myApp/src/main/java/com/rishith/myApp/Dev.java
@@ -1,11 +1,17 @@
 package com.rishith.myApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
 
+    @Autowired // it automatically connects it behind the scene
+    private Laptop laptop;
+
+
     public void build(){
+        laptop.compile();
         System.out.println("working on Awesome Project");
     }
 }
---
+++ myApp/src/main/java/com/rishith/myApp/Laptop.java
@@ -0,0 +1,11 @@
+package com.rishith.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    
+    public void compile(){
+        System.out.println("Compiling with 404 bugs");
+    }
+}
